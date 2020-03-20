fa3f5fd9491961c918304cf1fa4102d0c0d7f228
Nas1edNIK <Nikok@zos-v.ru>
2020-03-20 17:15:29 +0400

    new project has been created

---
+++ WebApplicationDotNetCore/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplicationDotNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //CreateWebHostBuilder(args).Build().Run();
+            using(var host = WebHost.Start("http://localhost:8080", 
+                context => context.Response.WriteAsync("Hello WebHost")))
+            {
+                Console.WriteLine("Applicatiion has been started");
+                host.WaitForShutdown();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
